5b8f2caf28889b8a5f2489b55e2823837847e2e0
sohatach <sohatach@gmail.com>
2018-11-12 22:18:22 +0900

    verup http://kotlinlang.org/docs/reference/annotations.html#arrays-as-annotation-parameters

--- src/main/kotlin/com/example/openidconnecttest/HelloController.kt
+++ src/main/kotlin/com/example/openidconnecttest/HelloController.kt
@@ -5,7 +5,7 @@ import org.springframework.security.oauth2.client.authentication.OAuth2Authentic
 import org.springframework.web.bind.annotation.*
 
 @RestController
-@RequestMapping(value = "oauthlogintest")
+@RequestMapping(value = ["oauthlogintest"])
 class HelloController {
     @GetMapping("/")
     fun hi(authentication: OAuth2AuthenticationToken): ResponseEntity<Map<String, Any>> {
